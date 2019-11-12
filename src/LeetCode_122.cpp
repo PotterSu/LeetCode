7060a2bb6fb9c3ef59c19bcec0967db569bb2b6a
sbw <sbw@meitu.com>
2019-11-12 20:25:05 +0800

    Add LeetCode_122.cpp

---
+++ src/LeetCode_122.cpp
@@ -0,0 +1,71 @@
+/*
+122. 买卖股票的最佳时机
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+
+设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+示例 1:
+
+输入: [7,1,5,3,6,4]
+输出: 7
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+     随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+示例 2:
+
+输入: [1,2,3,4,5]
+输出: 4
+解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+     注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。
+     因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+示例 3:
+
+输入: [7,6,4,3,1]
+输出: 0
+解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+*/
+#include "iostream"
+#include "string"
+#include "algorithm"
+#include "vector"
+
+using namespace std;
+
+int maxProfit(vector<int>& prices)
+{
+	if(prices.size() <= 1)
+	{
+		return 0;
+	}
+	vector<int> diff(prices.size() - 1);
+	for(int i = 0; i < diff.size(); i++)
+	{
+		diff[i] = prices[i+1] - prices[i];
+	}
+	vector<int> dp(prices.size());
+	dp[0] = 0;
+	int maxPrice = 0;
+	for(int i = 1; i < prices.size(); i++)
+	{
+		dp[i] = dp[i-1] + max(diff[i-1], 0);
+
+	}
+	maxPrice = dp[prices.size() - 1];
+	return maxPrice;
+}
+
+int main()
+{
+	vector<int> prices;
+	prices.push_back(7);
+	prices.push_back(1);
+	prices.push_back(5);
+	prices.push_back(3);
+	prices.push_back(6);
+	prices.push_back(4);
+	int result = maxProfit(prices);
+	cout << result << endl;
+
+	return 0;
+}
